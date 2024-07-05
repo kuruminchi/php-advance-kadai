0bc8dd6f831a46f5787e0d1896f3dee8564e4ad2
kuruminchi <166799565+kuruminchi@users.noreply.github.com>
2024-07-05 23:14:46 +0900

    Add files via upload

---
+++ kadai_016/php-book-app/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>書籍管理アプリ</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Google Fontsの読み込み -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <a href="index.php">書籍管理アプリ</a>
+      </nav>  
+    </header> 
+    <div class="wrapper">
+      <main>
+        <div id="index-main">
+          <h1>書籍管理アプリ</h1>
+          <p>『PHPとデータベースを連携しよう』成果物</p>
+        </div>
+        <div class="btn">
+          <a href="read.php">書籍一覧</a>
+        </div>  
+      </main>  
+      <footer>
+        <p>&copy; 書籍管理アプリ All rights reserved.</p>
+      </footer>    
+    </div>
+  </body>
+</html>   
